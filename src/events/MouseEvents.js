9600a2d932e84810be78ab372b3014bd7dd071f7
Josh Stendebach <jlstendebach@gmail.com>
2022-04-17 00:06:09 -0500

    Added support for mouse wheel events

--- src/events/MouseEvents.js
+++ src/events/MouseEvents.js
@@ -1,67 +1,76 @@
 export class MouseEvent {
-    constructor(x, y, btn, target = null) {
+    // event.which values: https://www.w3schools.com/jsref/event_which.asp
+    static LEFT = 0;
+    static MIDDLE = 1;
+    static RIGHT = 2;
+    static BACK = 3;
+    static FORWARD = 4;
+
+    constructor(x, y, button, target = null) {
         this.x = x;
         this.y = y;
-        this.btn = btn;
+        this.button = button;
         this.target = target;
     }
 }
 
-// event.which values: https://www.w3schools.com/jsref/event_which.asp
-MouseEvent.LEFT = 1;
-MouseEvent.MIDDLE = 2;
-MouseEvent.RIGHT = 3;
-
-
-// ==[ MouseDownEvent ]=========================================================
+// --[ MouseDownEvent ]---------------------------------------------------------
 export class MouseDownEvent extends MouseEvent {
-    constructor(x, y, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, button, target) {
+        super(x, y, button, target);
     }
 }
 
-// ==[ MouseUpInsideEvent ]=====================================================
+// --[ MouseUpInsideEvent ]-----------------------------------------------------
 export class MouseUpInsideEvent extends MouseEvent {
-    constructor(x, y, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, button, target) {
+        super(x, y, button, target);
     }
 }
 
-// ==[ MouseUpOutsideEvent ]====================================================
+// --[ MouseUpOutsideEvent ]----------------------------------------------------
 export class MouseUpOutsideEvent extends MouseEvent {
-    constructor(x, y, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, button, target) {
+        super(x, y, button, target);
     }
 }
 
-// ==[ MouseDragEvent ]=========================================================
+// --[ MouseDragEvent ]---------------------------------------------------------
 export class MouseDragEvent extends MouseEvent {
-    constructor(x, y, dx, dy, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, dx, dy, button, target) {
+        super(x, y, button, target);
         this.dx = dx;
         this.dy = dy;
     }
 }
 
-// ==[ MouseMoveEvent ]=========================================================
+// --[ MouseMoveEvent ]---------------------------------------------------------
 export class MouseMoveEvent extends MouseEvent {
-    constructor(x, y, dx, dy, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, dx, dy, button, target) {
+        super(x, y, button, target);
         this.dx = dx;
         this.dy = dy;
     }
 }
 
-// ==[ MouseEnterEvent ]========================================================
+// --[ MouseEnterEvent ]--------------------------------------------------------
 export class MouseEnterEvent extends MouseEvent {
-    constructor(x, y, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, button, target) {
+        super(x, y, button, target);
     }
 }
 
-// ==[ MouseExitEvent ]=========================================================
+// --[ MouseExitEvent ]---------------------------------------------------------
 export class MouseExitEvent extends MouseEvent {
-    constructor(x, y, btn, target) {
-        super(x, y, btn, target);
+    constructor(x, y, button, target) {
+        super(x, y, button, target);
+    }
+}
+
+// --[ MouseWheelEvent ]--------------------------------------------------------
+export class MouseWheelEvent extends MouseEvent {
+    constructor(x, y, button, amount, target) {
+        super(x, y, button, target);
+        this.amount = amount;
     }
 }
