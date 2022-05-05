6d366e7b0b4b739422b1eead1edb0e6ba3bdc21a
Josh Stendebach <jlstendebach@gmail.com>
2022-05-05 12:38:41 -0500

    Moved mouse and keyboard events

--- src/events/index.js
+++ src/events/index.js
@@ -1,7 +1,11 @@
 export * from "./AppEvents.js"
 export * from "./CanvasEvents.js"
 export * from "./EventEmitter.js"
-export * from "./Keyboard.js"
-export * from "./MouseButton.js"
-export * from "./MouseEventProcessor.js"
-export * from "./MouseEvents.js"
+
+// --[ keyboard ]---------------------------------------------------------------
+export * from "./keyboard/Keyboard.js"
+
+// --[ mouse ]------------------------------------------------------------------
+export * from "./mouse/MouseButton.js"
+export * from "./mouse/MouseEventProcessor.js"
+export * from "./mouse/MouseEvents.js"
