0b1d81745dca9f7b0d0baf8bc0f1fba78206a59b
Josh Stendebach <jlstendebach@gmail.com>
2026-06-10 00:36:05 -0500

    docs: add documentation comments to CanvasApp

---
+++ src/core/canvas-app/CanvasAppState.js
@@ -0,0 +1,6 @@
+export const CanvasAppState = Object.freeze({
+    STOPPED: 0,
+    RUNNING: 1,
+    DESTROYING: 2,
+    DESTROYED: 3
+});
--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,5 @@
 export * from "./canvas-app/CanvasApp.js";
-export * from "./canvas-app/ObservableCanvasApp.js";
 export * from "./canvas-app/CanvasAppEvent.js";
+export * from "./canvas-app/CanvasAppState.js";
+export * from "./canvas-app/ObservableCanvasApp.js";
+
