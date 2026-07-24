3253a19c50a619d05dcb5e07d53ea5a928d62bd3
Josh Stendebach <jlstendebach@gmail.com>
2026-07-24 16:59:14 -0500

    refactor: add private setParent helper function

--- src/graphics/views/core/View.js
+++ src/graphics/views/core/View.js
@@ -373,7 +373,7 @@ export class View {
 
         // Add the view.
         this.#views.push(view);
-        view.#parent = this;
+        view.#setParent(this);
 
         // The child view may have changed the bounds of this view.
         this.invalidateBounds();
@@ -396,15 +396,13 @@ export class View {
             // This should never happen because the view's parent reference 
             // indicates it is a child, but if it does, we still want to remove 
             // the parent reference.
-            view.#parent = null;
-            view.#parentWorldMatrixVersion = -1;
+            view.#setParent(null);
             return this;
         }
 
         // Remove the view.
         this.#views.splice(index, 1);
-        view.#parent = null;
-        view.#parentWorldMatrixVersion = -1;
+        view.#setParent(null);
 
         // The child view may have changed the bounds of this view.
         this.invalidateBounds();
@@ -417,8 +415,7 @@ export class View {
      */
     removeAllViews() {
         for (let i = 0; i < this.#views.length; i++) {
-            this.#views[i].#parent = null;
-            this.#views[i].#parentWorldMatrixVersion = -1;
+            this.#views[i].#setParent(null);
         }
         this.#views.length = 0;
         this.invalidateBounds();
@@ -678,4 +675,12 @@ export class View {
     onMouseEnter(event) { this.events.emit(event.type, event); }
     onMouseExit(event) { this.events.emit(event.type, event); }
     onMouseWheel(event) { this.events.emit(event.type, event); }
+
+    // -------------------------------------------------------------------------
+    // MARK: - Helpers
+    // -------------------------------------------------------------------------
+    #setParent(parent) {
+        this.#parent = parent;
+        this.#parentWorldMatrixVersion = -1;
+    }
 }
