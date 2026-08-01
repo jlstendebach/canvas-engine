fb1775ec58e1fa9e9449649a733207c06ca70d96
Josh Stendebach <jlstendebach@gmail.com>
2026-08-01 16:44:14 -0500

    test: add test stubs for View hierarchy methods

---
+++ tests/graphics/views/core/View.test.js
@@ -0,0 +1,69 @@
+import { View } from "@canvas-engine";
+import { describe, expect, test } from "@jest/globals";
+
+describe("View", () => {
+
+    // -------------------------------------------------------------------------
+    // MARK: - Parent Management 
+    // -------------------------------------------------------------------------
+
+    describe("addToParent(parent)", () => {
+    });
+
+    describe("removeFromParent()", () => {
+    });
+
+    describe("sendToBack()", () => {
+    });
+
+    describe("bringToFront()", () => {
+    });
+
+    // -------------------------------------------------------------------------
+    // MARK: - Child Management 
+    // -------------------------------------------------------------------------
+
+    describe("addView(view)", () => {
+    });
+
+    describe("addViewAt(view, index)", () => {
+    });
+
+    describe("removeView(view)", () => {
+    });
+
+    describe("removeViewAt(index)", () => {
+    });
+
+    describe("removeAllViews()", () => {
+    });
+
+    describe("getViews()", () => {
+    });
+
+    describe("getViewAt(index)", () => {
+    });
+
+    describe("getViewCount()", () => {
+    });
+
+    describe("getViewIndex(view)", () => {
+    });
+
+    describe("setViewIndex(view, index)", () => {
+    });
+
+    describe("hasView(view)", () => {
+    });
+
+    // -------------------------------------------------------------------------
+    // MARK: - Hierarchy Queries
+    // -------------------------------------------------------------------------
+
+    describe("isDescendantOf(view)", () => {
+    });
+
+    describe("isAncestorOf(view)", () => {
+    });
+
+});
