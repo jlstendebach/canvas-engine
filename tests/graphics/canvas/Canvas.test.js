6679c1787bb84bfdf61b7bf147df1ab96403f211
Josh Stendebach <jlstendebach@gmail.com>
2026-08-06 10:29:02 -0500

    test: add Canvas child management test stubs

---
+++ tests/graphics/canvas/Canvas.test.js
@@ -0,0 +1,43 @@
+import { Canvas } from "@canvas-engine";
+import { jest, describe, expect, test } from "@jest/globals";
+
+describe("Canvas", () => {
+
+    // -------------------------------------------------------------------------
+    // MARK: - Child Management
+    // -------------------------------------------------------------------------
+
+    describe("addView(view)", () => {
+    });
+
+    describe("addViewAt(view, index)", () => {
+    });
+
+    describe("removeView(view)", () => {
+    });
+
+    describe("removeViewAt(index)", () => {
+    });
+
+    describe("removeAllViews()", () => {
+    });
+
+    describe("getViews()", () => {
+    });
+
+    describe("getViewAt(index)", () => {
+    });
+
+    describe("getViewCount()", () => {
+    });
+
+    describe("getViewIndex(view)", () => {
+    });
+
+    describe("setViewIndex(view, index)", () => {
+    });
+
+    describe("hasView(view)", () => {
+    });
+
+});
