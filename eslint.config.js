7c40c6f11653fe0890a3034058d3ffe4ba82f388
Josh Stendebach <jlstendebach@gmail.com>
2026-09-09 17:23:52 -0500

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -22,8 +22,7 @@ export default defineConfig([
             "@typescript-eslint/no-unused-vars": [
                 "warn",
                 {
-                    args: "all",
-                    argsIgnorePattern: "^_$",
+                    args: "none", // Signatures are often fixed by overrides and callbacks
                     varsIgnorePattern: "^_$",
                     caughtErrors: "all",
                     caughtErrorsIgnorePattern: "^_$",
