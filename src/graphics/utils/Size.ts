eae645c1c2b38f1d8a6fa5b5d14dab8c3a9e60d3
Josh Stendebach <jlstendebach@gmail.com>
2026-09-14 20:06:53 -0500

    refactor(Size): type the behavior

--- src/graphics/utils/Size.ts
+++ src/graphics/utils/Size.ts
@@ -1,17 +1,17 @@
 import { Vec2 } from "../../math/Vec2.js";
 
 export class Size extends Vec2 {
-    get width() { 
+    get width(): number { 
         return this.x; 
     }
-    set width(value) { 
+    set width(value: number) { 
         this.x = value; 
     }
 
-    get height() { 
+    get height(): number { 
         return this.y; 
     }
-    set height(value) { 
+    set height(value: number) { 
         this.y = value; 
     }
 }
